a8d4ae4d3cd5ffcd7c2f9ba6019bcc4aea50be71
irb1s <kolyirb1s@gmail.com>
2022-10-06 15:44:48 +0500

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+
+import AppRouter from "./router/AppRouter";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+    const [data, setData] = useState(null);
+    const [toggle, setToggle] = useState(false);
+    const [value, setValue] = useState('')
+
+    useEffect(() => {
+        setTimeout(() => {
+            setData({})
+        }, 100)
+    });
+
+    const handleClick = () => {
+        setToggle((prevState) => !prevState)
+    }
+
+    return (
+        <div>
+            {toggle && <div data-testid='toggled-item'>"toggled"</div>}
+            {data && <div>data</div>}
+            <button onClick={handleClick} data-testid='toggle-btn'>click on me</button>
+            render app
+            <div data-testid='input-value'>{value}</div>
+            <input data-testid='input' onChange={(e) => setValue(e.target.value)} type='text' placeholder='input'/>
+            <Navbar/>
+            <AppRouter/>
+        </div>
+    );
+};
+
+export default App;
